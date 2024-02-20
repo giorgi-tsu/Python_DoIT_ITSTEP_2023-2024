4ac7aaf6f42eb06b9a1b5cae4ae4ef36a869e01a
giorgi-tsu <gtechttech@gmail.com>
2024-02-20 22:39:32 +0400

    9 lines written

---
+++ Lectures/L-23-2024-02-19/mini_flask_app/main.py
@@ -0,0 +1,9 @@
+# ჩვენს შემთხვევაში მოცემული main.py ასრულებს controller-ის
+# ფუნქციას.
+
+# Importing libraries
+from flask import Flask
+
+# Creating Flask instance
+# app = Flask(__name__)
+print(__name__)
