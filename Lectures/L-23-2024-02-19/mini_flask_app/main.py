cb99a8741d825bf687ffba95ca9cd107e190122e
giorgi-tsu <gtechttech@gmail.com>
2024-02-21 22:53:08 +0400

    first page

--- Lectures/L-23-2024-02-19/mini_flask_app/main.py
+++ Lectures/L-23-2024-02-19/mini_flask_app/main.py
@@ -6,6 +6,12 @@ from flask import Flask
 
 # Creating Flask instance
 app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "<h1>Hello I am here</h1>"
+
+
 app.run()
 
 # 2024-02-19_00-34-18
