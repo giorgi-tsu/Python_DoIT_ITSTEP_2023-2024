9d9762fd504c729a392d0e582ed1d7a365370c22
giorgi-tsu <gtechttech@gmail.com>
2024-02-21 23:09:53 +0400

    about page added

--- Lectures/L-23-2024-02-19/mini_flask_app/main.py
+++ Lectures/L-23-2024-02-19/mini_flask_app/main.py
@@ -12,9 +12,13 @@ def home():
     # return "<h1>Hello I am here</h1>"
     return "Hello I am here"
 
+@app.route("/about")
+def about():
+    return "<h1>About</h1>"
 
 
 if __name__ == "__main__":
     app.run(debug=True) # თუ ამას დავუწერ მაშინ დარეფრეშებითაც 
                     # აღიქვამს ცვლილებებს.
+
 
