2c6ae7df2480668bc45fa77af00d1f8ee29c6d1d
giorgi-tsu <gtechttech@gmail.com>
2024-02-26 17:52:11 +0400

    Homework 23 added

--- Homeworks/დავალება 23/mini_flask_app/main.py
+++ Homeworks/დავალება 23/mini_flask_app/main.py
@@ -1,12 +1,5 @@
-# ჩვენს შემთხვევაში მოცემული main.py ასრულებს controller-ის
-# ფუნქციას.
-
 # Importing libraries
-from flask import Flask, render_template
-
-from models import ProductModel 
-
-# render_template გვაძლევს საშუალებას დავარენდეროთ HTML-ის გვერდი.
+from flask import Flask, render_template 
 
 # Creating Flask instance
 app = Flask(__name__)
@@ -14,26 +7,18 @@ app = Flask(__name__)
 @app.route("/")
 @app.route("/home")
 def home():
-    # return "<h1>Hello I am here</h1>"
-    # return "Hello I am here"
     return render_template("home.html")
 
 @app.route("/about")
 def about():
-    # ვითომ ინფორმაცია მოგვაქვს მონაცემთა ბაზიდან
-    obj1 = ProductModel("Red Bull Zero", "0% Sugar", 5 )
-    obj2 = ProductModel("Red Energy Drink", "Vitalizes Body and Mind", 6)
-    obj3 = ProductModel("Red Bull Sugarfree", "Something", 7)
-    
-    obj_list = [obj1, obj2, obj3]
+    return render_template("about.html")
 
-    return render_template("about.html", obj_list=obj_list)
-
-    # return "<h1>About</h1>"
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
 
 if __name__ == "__main__":
-    app.run(debug=True) # თუ ამას (debug) დავუწერ, მაშინ
-    # დარეფრეშებითაც აღიქვამს ცვლილებებს.
+    app.run(debug=True)
 
 
 
--- Homeworks/დავალება 23/mini_flask_app/models.py
+++
@@ -1,7 +0,0 @@
-class ProductModel:
-    def __init__(self, name, details, price):
-        self.name = name
-        self.details = details
-        self.price = price
-
-        
