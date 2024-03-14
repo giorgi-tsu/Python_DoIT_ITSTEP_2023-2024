037720f99759ec485e0d0da5561ee93b38de1351
giorgi-tsu <gtechttech@gmail.com>
2024-03-14 12:00:53 +0400

    Lecture 27 done

--- Lectures/L-27_2024-03-14/exercises.py
+++ Lectures/L-27_2024-03-14/exercises.py
@@ -1,15 +1,36 @@
 import unittest
 
-def equal_lists(list1, list2):
-    return list1 == list2
+# def equal_lists(list1, list2):
+#     return list1 == list2
 
 
-class TestListsEquality(unittest.TestCase):
-    def test_equal_lists(self):
-        list1 = [2, 4, 7]
-        list2 = [2, 4, 7]
+# class TestListsEquality(unittest.TestCase):
+#     def test_equal_lists(self):
+#         list1 = [2, 4, 7]
+#         list2 = [2, 4, 7]
 
-        self.assertTrue(equal_lists(list1, list2), "The lists are different")
+#         self.assertTrue(equal_lists(list1, list2),
+#                         "The lists are different")
+
+
+#         list1 = [2, 4, 7]
+#         list2 = [2, 4, 7]
+
+#         self.assertFalse(equal_lists(list1, list2),
+#                          "The lists are not different")
+
+
+def is_palindrome(word):
+    return word == word[::-1]
+
+
+class TestIfPolindrome(unittest.TestCase):
+    
+    def test_is_palindrome(self):
+        word = "aba"
+        self.assertTrue(is_palindrome(word), "Not palindrome")
+
+
 
 if __name__ == "__main__":
     unittest.main()
