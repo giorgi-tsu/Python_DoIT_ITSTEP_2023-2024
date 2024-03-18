15af21a01fe3423d0609394b3fb00dc81bd74179
giorgi-tsu <gtechttech@gmail.com>
2024-03-18 13:19:55 +0400

    Eveything works untill now

--- Lectures/L-27_2024-03-14/additional_resources/test_calc.py
+++ Lectures/L-27_2024-03-14/additional_resources/test_calc.py
@@ -19,12 +19,14 @@ class TestCalc(unittest.TestCase):
         self.assertEqual(calc.multiply(10, 5), 50)
         self.assertEqual(calc.multiply(-1, 1), -1)
         self.assertEqual(calc.multiply(-1, -1), 1)
-        self.assertEqual(calc.multiply(5, 2), 2.5)
+        
 
     def test_divide(self):
         self.assertEqual(calc.divide(10, 5), 2)
         self.assertEqual(calc.divide(-1, 1), -1)
         self.assertEqual(calc.divide(-1, -1), 1)
+        self.assertEqual(calc.divide(5, 2), 2.5)
+
 
 if __name__ == "__main__":
     unittest.main()
