0dc743adaab53728334be1daef906eeb2c4aeb3c
giorgi-tsu <gtechttech@gmail.com>
2024-03-18 13:33:38 +0400

    Checking ValueError Method 1

--- Lectures/L-27_2024-03-14/additional_resources/calc.py
+++ Lectures/L-27_2024-03-14/additional_resources/calc.py
@@ -19,5 +19,3 @@ def divide(x, y):
     if y == 0:
         raise ValueError('Can not divide by zero!')
     return x / y
-
-  
--- Lectures/L-27_2024-03-14/additional_resources/test_calc.py
+++ Lectures/L-27_2024-03-14/additional_resources/test_calc.py
@@ -26,6 +26,7 @@ class TestCalc(unittest.TestCase):
         self.assertEqual(calc.divide(-1, 1), -1)
         self.assertEqual(calc.divide(-1, -1), 1)
         self.assertEqual(calc.divide(5, 2), 2.5)
+        self.assertRaises(ValueError, calc.divide, 10, 0)
 
 
 if __name__ == "__main__":
