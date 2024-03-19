8ed2318334b959d240d85ea123cdf827580cf500
giorgi-tsu <giorgi.tsu@gmail.com>
2024-03-19 12:37:20 +0400

    homework 27 DONE!

--- Homeworks/hw-27_2024-03-14/test_prime_checker.py
+++ Homeworks/hw-27_2024-03-14/test_prime_checker.py
@@ -2,13 +2,21 @@ import unittest
 from prime_checker import is_prime
 
 class TestPrime_checker(unittest.TestCase):
+
     
     def test_is_prime(self):
-        self.assertTrue(is_prime(2), "2 is Prime")
-        self.assertTrue(is_prime(3), "2 is Prime")
-        self.assertFalse(is_prime(4), "4 is NOT Prime")
-        self.assertTrue(is_prime(5), "2 is Prime")
-        self.assertFalse(is_prime(6), "4 is NOT Prime")
+        
+        list_of_primes = [2, 5, 7, 11, 29]
+        list_of_nonprimes = [4, 8, 12, 14, 28]
+
+
+        for i in list_of_primes:
+            self.assertTrue(is_prime(i), f"{i} is Prime")
+
+
+        for i in list_of_nonprimes:
+            self.assertFalse(is_prime(i), f"{i} is NOT Prime")
+
 
 if __name__ == "__main__":
     unittest.main()
--- Lectures/L-27_2024-03-14/exercises.py
+++ Lectures/L-27_2024-03-14/exercises.py
@@ -1,34 +1,34 @@
 import unittest
 
-# def equal_lists(list1, list2):
-#     return list1 == list2
+def equal_lists(list1, list2):
+    return list1 == list2
 
 
-# class TestListsEquality(unittest.TestCase):
-#     def test_equal_lists(self):
-#         list1 = [2, 4, 7]
-#         list2 = [2, 4, 7]
+class TestListsEquality(unittest.TestCase):
+    def test_equal_lists(self):
+        list1 = [2, 4, 7]
+        list2 = [2, 4, 7]
 
-#         self.assertTrue(equal_lists(list1, list2),
-#                         "The lists are different")
-
-
-#         list1 = [2, 4, 7]
-#         list2 = [2, 4, 7]
-
-#         self.assertFalse(equal_lists(list1, list2),
-#                          "The lists are not different")
+        self.assertTrue(equal_lists(list1, list2),
+                        "The lists are different")
 
 
-def is_palindrome(word):
-    return word == word[::-1]
+        list1 = [2, 4, 7]
+        list2 = [2, 4, 8]
+
+        self.assertFalse(equal_lists(list1, list2),
+                         "The lists are equal")
 
 
-class TestIfPolindrome(unittest.TestCase):
+# def is_palindrome(word):
+#     return word == word[::-1]
+
+
+# class TestIfPolindrome(unittest.TestCase):
     
-    def test_is_palindrome(self):
-        word = "aba"
-        self.assertTrue(is_palindrome(word), "Not palindrome")
+#     def test_is_palindrome(self):
+#         word = "aba"
+#         self.assertTrue(is_palindrome(word), "Not palindrome")
 
 
 
