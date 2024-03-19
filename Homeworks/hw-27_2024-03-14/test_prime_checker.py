abf9a1c0a056a7f32cb0fc714fd2400f00298efe
giorgi-tsu <giorgi.tsu@gmail.com>
2024-03-19 12:25:54 +0400

    version 2 of homework 27 done

--- Homeworks/hw-27_2024-03-14/test_prime_checker.py
+++ Homeworks/hw-27_2024-03-14/test_prime_checker.py
@@ -4,11 +4,11 @@ from prime_checker import is_prime
 class TestPrime_checker(unittest.TestCase):
     
     def test_is_prime(self):
-        self.assertEqual(is_prime(2), True)
-        self.assertEqual(is_prime(3), True)
-        self.assertEqual(is_prime(4), False)
-        self.assertEqual(is_prime(5), True)
-        self.assertEqual(is_prime(6), False)
+        self.assertTrue(is_prime(2), "2 is Prime")
+        self.assertTrue(is_prime(3), "2 is Prime")
+        self.assertFalse(is_prime(4), "4 is NOT Prime")
+        self.assertTrue(is_prime(5), "2 is Prime")
+        self.assertFalse(is_prime(6), "4 is NOT Prime")
 
 if __name__ == "__main__":
     unittest.main()
