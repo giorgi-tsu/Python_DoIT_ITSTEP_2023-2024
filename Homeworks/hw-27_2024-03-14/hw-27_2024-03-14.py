178798981c39197b196338d44968c9af7bfa19b7
giorgi-tsu <giorgi.tsu@gmail.com>
2024-03-19 11:46:26 +0400

    Homework directory updated according to the new naming convention

---
+++ Homeworks/hw-27_2024-03-14/hw-27_2024-03-14.py
@@ -0,0 +1,12 @@
+
+# ფუნქცია ამოწმებს მარტივია თუ არა რიცხვი. 
+# (მარტივია რიცხვი თუ ის მხოლოდ 1_ზე და საკუთარ თავზე იყოფა)
+def is_prime(number):
+    if number < 2:
+        return False
+    for i in range(2, int(number**0.5) + 1):
+        if number % i == 0:
+            return False
+    return True
+
+# გატესტე ფუნქცია'unittest'_ის დახმარებით.
