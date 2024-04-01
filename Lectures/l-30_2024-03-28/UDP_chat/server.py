71ca5829b6996597eed112391ed2c84b7851af01
giorgi-tsu <giorgi.tsu@gmail.com>
2024-04-01 16:53:38 +0400

    receive added tos erver.py

--- Lectures/l-30_2024-03-28/UDP_chat/server.py
+++ Lectures/l-30_2024-03-28/UDP_chat/server.py
@@ -13,4 +13,36 @@ clients = []
 host = "127.0.0.1"
 port = 55555
 
+# ვქმნით სერვერს
 
+# AF_INET ნიშნავს, რომ ვიყენებთ IPv4 ოჯახს. 
+# SOCK_DGRAM ნიშნავს, რომ ეს არის UDP პროტოკოლი
+
+server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+# სერვერის გაშვება
+server.bind((host, port))
+
+# ვქმნით ფუნქციას, რომელიც მიიღებს შეტყობინებებს
+
+def receive():
+    print("Server Started!")
+    while True:
+        try:
+            # სერვერი მიიღებს შეტყობინებას და კლიენტის მისამართს
+            message, addr = server.recvfrom(1024)
+            # ვინახავთ შეტყობინებას "ბაზაში"
+            messages.put((message, addr))
+
+        
+        except:
+            pass
+
+
+# ვქმნით ფუნქციას, რომელიც ყველა კლიენტს გაუგზავნის ერთის მიერ 
+# გამოგზავნილ შეტყობინებას
+        
+def broadcast():
+    while True:
+        while not messages.empty():
+            message, addr = messages.get()
