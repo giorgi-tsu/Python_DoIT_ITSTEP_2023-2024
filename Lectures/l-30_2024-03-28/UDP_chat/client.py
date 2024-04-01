885dd0a101fd3480ff8b8a964548d6d77936fa40
giorgi-tsu <giorgi.tsu@gmail.com>
2024-04-01 17:15:20 +0400

    client.py ready until recceive fucntion

--- Lectures/l-30_2024-03-28/UDP_chat/client.py
+++ Lectures/l-30_2024-03-28/UDP_chat/client.py
@@ -1,3 +1,19 @@
 import socket
 import threading
 import random
+
+
+
+host = "127.0.0.1"
+port = 55555    
+
+client_port = random.randint(8000, 9000)
+
+client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+client.bind((host, client_port))
+
+nickname = input("Nickname: ")
+
+def receieve():
+    
