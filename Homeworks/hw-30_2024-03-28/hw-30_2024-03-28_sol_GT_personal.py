5b38a200d552a57202a2410bf0336cf849e8b6b8
giorgi-tsu <giorgi.tsu@gmail.com>
2024-04-02 22:55:54 +0400

    homework 30 done!

--- Homeworks/hw-30_2024-03-28/hw-30_2024-03-28_sol_GT_personal.py
+++ Homeworks/hw-30_2024-03-28/hw-30_2024-03-28_sol_GT_personal.py
@@ -12,5 +12,39 @@ print("\n",
 
 ## სავარჯიშო 1
 
+# 1. შექმენი ასინქრონული event loop რომელიც ერთი წამის დაყოვნებით
+# ამობეჭდავს რიცხვებს 1_დან 10_ის ჩათვლით.
+
+# საჭირო ბიბლიოთეკების შემოტანა
+
+import asyncio
+
+async def event_loop():
+    for i in range(1, 11):
+        print(i)
+        await asyncio.sleep(1)      
+
+asyncio.run(event_loop())
+
+print("\n",
+      "-------------------- სავარჯიშო 2 --------------------", "\n")
+
+## სავარჯიშო 2
+
+# 2. დაწერე ასინქრონული ფუნქციის კოდი, რომელიც მოიცდის ორი წამი
+# და შემდეგ დაბეჭდავს "Hello World!!!" 
+# შექმენი event loop რომელიც ზემოხსენებულ ფუნქციას გაუშვებს.
+
+
+async def wait_2_sec():
+    await asyncio.sleep(2)
+    print("Hello World!!!")
+
+async def event_loop2():
+    await wait_2_sec()
+
+asyncio.run(event_loop2())
+
+
 
 #------------------------------------------------------------------#
