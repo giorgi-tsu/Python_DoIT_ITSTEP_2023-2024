49e50bdb71313ad59f5143b00de56707fd6a29a9
giorgi-tsu <giorgi.tsu@gmail.com>
2024-04-08 15:22:29 +0400

    lecture 31 done

--- Lectures/l-31_2024-04-01/l-31_2024-04-01_part2.sql
+++ Lectures/l-31_2024-04-01/l-31_2024-04-01_part2.sql
@@ -1,5 +1,18 @@
 use it_step;
 
+select * from customers;
+
+select customerName, city from customers;
+
+select * from customers limit 5;
+
+select * from customers where city = "NYC";
+	
+select * from customers where city != "NYC";
+
+select customerName, creditLimit from customers where creditLimit < ;
+
+
 /* select * from customers; */
 
 select customerName, city from customers;
