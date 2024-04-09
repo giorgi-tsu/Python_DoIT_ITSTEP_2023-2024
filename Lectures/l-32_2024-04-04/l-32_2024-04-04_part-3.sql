35f61ddcd193e8f2e5fc3eae5eff6eb17d5eab6d
giorgi-tsu <giorgi.tsu@gmail.com>
2024-04-09 18:07:20 +0400

    Lecture 32 timestamp: 01-02-00. One to Many relationship done

--- Lectures/l-32_2024-04-04/l-32_2024-04-04_part-3.sql
+++ Lectures/l-32_2024-04-04/l-32_2024-04-04_part-3.sql
@@ -22,14 +22,29 @@ select * from categories;
 
 create table products
 (
-	categoryID int auto_increment not null primary key,
+	productID int auto_increment not null primary key,
     name varchar(50) not null,
     price int not null,
     categoryID int not null,
     foreign key (categoryID) references categories (categoryID)
 );
 
+select * from products;
+
 insert into categories
 (name)
+value
+("Electronics"),
+("Food");
+
+select * from categories;
+
+insert into products
+(name, price, categoryID)
 values
-()
+("Apple", 2, 2),
+("Milk", 1, 2),
+("TV", 400, 1),
+("Laptop", 3800, 1);
+
+select * from products
