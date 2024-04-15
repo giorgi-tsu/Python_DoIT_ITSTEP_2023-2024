10a08b0fe0fe5ed7999d04669a478f35f7b234e1
giorgi-tsu <giorgi.tsu@gmail.com>
2024-04-15 16:06:28 +0400

    Homework 33 done!

--- Homeworks/hw-33_2024-04-08/hw-33_2024-04-08.sql
+++ Homeworks/hw-33_2024-04-08/hw-33_2024-04-08.sql
@@ -1,6 +1,22 @@
 /*  დავალება 33 */
 
 /*
-1. 1.	IT_Step_ის მონაცემთა ბაზაში, პროდუქტების ცხრილში, აგრეგაციის ფუნქციის დახმარებით გამოთვალე პროდუქტის საშულო ფასი.
+1. IT_Step_ის მონაცემთა ბაზაში, პროდუქტების ცხრილში, აგრეგაციის ფუნქციის დახმარებით გამოთვალე პროდუქტის საშულო ფასი.
 */
+
+use it_step;
+
+select productName, avg(buyPrice) from products group by productName; -- პროდუქტის საშუალო ფასი თითოეული პროდუქტისთვის.
+
+select avg(buyPrice) as average_price from products; -- ყველა პროდუქტის საშუალო ფასი.
+
+
+/*
+2.	მონიშნე ყველა ის პროდუქტი, რომლის ფასიც საშუალოზე მაღალია.
+*/
+
+select productName, buyPrice from products where buyPrice > (select avg(buyPrice) from products) order by buyPrice; -- საშუალო ფასია: '54.395182' და
+-- მართლაც ვხედავთ რომ გამაოქვს ყველა პროდუტი, სადაც ფასი საშუალოზე მეტია.
+
+
 
