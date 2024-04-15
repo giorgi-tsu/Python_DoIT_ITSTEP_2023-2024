b5fce6d84d594a83045a9bc251c71021a5b1c5a8
giorgi-tsu <giorgi.tsu@gmail.com>
2024-04-15 10:24:49 +0400

    Lecturee 34 morning: wriitn code

--- Lectures/l-35_2024-04-15/l-35_2024-04-15.py
+++ Lectures/l-35_2024-04-15/l-35_2024-04-15.py
@@ -4,7 +4,29 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 
 from sqlalchemy import ForeignKey, Column, Integer, String, CHAR
 
+from random import choice
+
 Base = declarative_base()
 
 class Person(Base):
+    __tablename__ = "Persons"
+
+    id = Column("id", Integer, primary_key=True)
+    name = Column("name", String)
+    age = Column("age", Integer)
+
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
     
+    def __repr__(self):
+        return f"Person(name={self.name})"
+    
+engine = create_engine("sqlite:///")
+
+Base.metadata.create_all(bind=engine)
+
+name = ["Giorgi", "Mariami", "Elene"]
+age = [22, 23, 18]
+
+
