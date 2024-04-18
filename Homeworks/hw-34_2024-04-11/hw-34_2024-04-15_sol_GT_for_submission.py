e168351f27a55e43ee285e6cc45d10ec1c472607
giorgi-tsu <giorgi.tsu@gmail.com>
2024-04-18 09:56:32 +0400

    homework 34 done!

--- Homeworks/hw-34_2024-04-11/hw-34_2024-04-15_sol_GT_for_submission.py
+++ Homeworks/hw-34_2024-04-11/hw-34_2024-04-15_sol_GT_for_submission.py
@@ -13,9 +13,16 @@ from sqlalchemy import Integer, String, CHAR
 
 # 1. sqlalchemy_ს გამოყენებით შექმენი sqlite_ის info.db ფაილი.
 
-os.chdir("./homeworks/hw-34_2024-04-11")
+os.chdir("./homeworks/hw-34_2024-04-11/dbs/sqlite")
+
+print(os.getcwd())
 
 Base = declarative_base()
+
+database_path = "info.db" 
+
+if os.path.exists(database_path):
+    os.remove(database_path)
 
 engine = create_engine("sqlite:///info.db")
 
@@ -27,14 +34,15 @@ Base.metadata.create_all(bind=engine)
 class Course(Base):
     __tablename__ = "courses"
 
-    course_id = Column("id", Integer, primary_key=True)
+    course_id = Column("id", Integer, primary_key=True, \
+                       autoincrement=True)
     name = Column("name", String)
 
     def __init__(self, name):
         self.name = name
 
     def __repr__(self):
-        return f"Courses(name={self.name})"
+        return f"Courses(id = {self.course_id}, name={self.name})"
 
   
 class Student(Base):
@@ -48,7 +56,7 @@ class Student(Base):
 
     def __init__(self, name, course_id):
         self.name = name
-        self.course = course_id
+        self.course_id = course_id
     
     def __repr__(self):
         return f"Students(name={self.name}, course={self.course_id})"
@@ -69,10 +77,35 @@ courses_model = []
 for c in courses:
     courses_model.append(Course(c))
 
-session.add_all(courses_model)
+# print(courses_model)
 
-# 4.	გამოიტანე ცხრილში არსებული ინფორმაცია __repr__ 
+session.add_all(courses_model)
+session.commit()
+
+student_names = ["Ilia", "Ako", "Gabo", "Gio", "Papuna", "Rezo",\
+                 "Semi", "Bacho", "Buta", "Geno", "Joni", "Ana"]
+student_course_id = [1, 2, 3, 5, 6]
+
+students_model = []
+
+for sn in student_names:
+    for sci in student_course_id:
+        students_model.append(Student(sn, sci))
+
+# print(students_model)
+
+session.add_all(students_model)
+session.commit()
+
+# 4. გამოიტანე ცხრილში არსებული ინფორმაცია __repr__ 
 # მეთოდის დხმარებით.
+
+all_courses = session.query(Course).all()
+print(all_courses)
+
+
+all_students = session.query(Student).all()
+print(all_students)
 
 
 #------------------------------------------------------------------#
--- Lectures/l-35_2024-04-15/l-35_2024-04-15_scratch.py
+++ Lectures/l-35_2024-04-15/l-35_2024-04-15_scratch.py
@@ -1,5 +1,46 @@
+import os
+from sqlalchemy import create_engine
+from sqlalchemy.orm import declarative_base, sessionmaker
+from sqlalchemy import Column, ForeignKey
+from sqlalchemy import Integer, String, CHAR 
 from random import choice
 
-names = ["Giorgi", "Mariami", "Elene", "Nika", "Davit"]
-ages = [22, 23, 18, 31, 40]
 
+Base = declarative_base()
+
+
+class Person(Base):
+    __tablename__ = "persons"
+
+    id = Column("id", Integer, primary_key=True)
+    name =  Column("name", String)  # ქმნის ცხრილის სვეტს.
+    age = Column("age", Integer)  # ქმნის ცხრილის სვეტს.
+
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def __repr__(self):
+        return f"Person(name={self.name}, age={self.age})"
+
+
+os.chdir("./lectures/l-35_2024-04-15/dbs/sqlite")
+print(os.getcwd())
+
+database_path = "mydb.db"
+if os.path.exists(database_path ):
+    os.remove(database_path)
+
+engine = create_engine("sqlite:///mydb.db") 
+
+Base.metadata.create_all(bind=engine)
+
+Session = sessionmaker(bind=engine)
+
+session = Session()
+
+person = Person("Levani", 15)
+
+session.add(person)
+
+print(session.query(Person).all())
