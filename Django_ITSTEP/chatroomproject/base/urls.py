ad969a440e4b8be1d51700e9ac03ad00a8eb5427
giorgi-tsu <giorgi.tsu@gmail.com>
2024-04-27 23:19:54 +0400

    names add

--- Django_ITSTEP/chatroomproject/base/urls.py
+++ Django_ITSTEP/chatroomproject/base/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.home),
-    path('room/', views.room)
+    path('', views.home, name='home'),
+    path('room/', views.room, name=room)
 ]
