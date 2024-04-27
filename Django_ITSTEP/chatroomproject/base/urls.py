589b260d74b77b82b24f6f3b877b62ffd7ce12db
giorgi-tsu <giorgi.tsu@gmail.com>
2024-04-27 23:18:45 +0400

    FUnctions moved to views

--- Django_ITSTEP/chatroomproject/base/urls.py
+++ Django_ITSTEP/chatroomproject/base/urls.py
@@ -1,14 +1,8 @@
 from django.urls  import path
-from django.http import HttpResponse
-
-def home(request):
-    return HttpResponse("<h1>Home Page</h1>")
-
-def room(request):
-    return HttpResponse('<h1>Rooms Page</h1>')
+from . import views
 
 
 urlpatterns = [
-    path('', home),
-    path('room/', room)
+    path('', views.home),
+    path('room/', views.room)
 ]
