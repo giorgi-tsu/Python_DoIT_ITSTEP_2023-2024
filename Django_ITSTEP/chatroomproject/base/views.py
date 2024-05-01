0227a96c50c583f5e1a889f4bdb5ce01972feab3
giorgi-tsu <giorgi.tsu@gmail.com>
2024-05-01 17:57:02 +0400

    add dictionary in home

--- Django_ITSTEP/chatroomproject/base/views.py
+++ Django_ITSTEP/chatroomproject/base/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
-# Create your views here.
+
+rooms = [
+    {'id': 1, "name": "Python SOLID Principles" },
+    {'id': 2, "name": "Python Formatting"},
+    {'id': 3, "name": "Python OOP"}
+]
 
 
 def home(request):
-    return render(request,'base/home.html')
+    return render(request,'base/home.html', {"rooms": rooms})
 
 def room(request):
     return render(request,'base/room.html')
