990f0141ae1ca5bfefa0f432f7081a81b78ecd98
giorgi-tsu <giorgi.tsu@gmail.com>
2024-05-01 23:26:42 +0400

    l-38_2024-04-25_01-25-31

--- Django_ITSTEP/chatroomproject/base/models.py
+++ Django_ITSTEP/chatroomproject/base/models.py
@@ -21,3 +21,13 @@ class Room(models.Model):
 
     def __str__(self):
         return self.name
+
+class Messages(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+    body = models.TextField()
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __repr__(self):
+        return self.body[0:50]
